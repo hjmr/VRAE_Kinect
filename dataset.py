a2070784155d1687050482f78c04a6e1b76277cc
Hajime Murao <2620990+hjmr@users.noreply.github.com>
2020-02-05 22:54:43 +0900

    add dataset loader

---
+++ dataset.py
@@ -0,0 +1,33 @@
+import json
+import argparse
+
+
+def flatten_skeleton_data(skel_data):
+    data_flatten = []
+    for a_time_data in skel_data:
+        entry = [0] * (len(a_time_data['joints']) * 3)  # num of joints * x, y, z
+        for joint in a_time_data['joints']:
+            entry[joint['id'] * 3 + 0] = joint['x']
+            entry[joint['id'] * 3 + 1] = joint['y']
+            entry[joint['id'] * 3 + 2] = joint['z']
+        data_flatten.append(entry)
+    return data_flatten
+
+
+def get(file_name):
+    data_flatten = []
+    with open(file_name, "r") as _f:
+        data_flatten = flatten_skeleton_data(json.load(_f))
+    return data_flatten
+
+
+def parse_arg():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('data_file', type=str, nargs='+', help='file name(s) of skeleton data.')
+    return parser.parse_args()
+
+
+if __name__ == '__main__':
+    args = parse_arg()
+    for fn in args.data_file:
+        data_flatten = get(fn)
