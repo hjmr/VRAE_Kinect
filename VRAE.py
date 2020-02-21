correct loss calculation.

VRAE.py
@@ -4,7 +4,7 @@ import torch
 import torch.nn as nn
 from torch.nn.utils.rnn import pack_padded_sequence, pad_packed_sequence
 
-from utils import make_ones, ensure_padded_sequence
+from utils import make_padded_ones, make_unpadded_sequence
 
 
 class VRAE(nn.Module):
@@ -39,7 +39,7 @@ class VRAE(nn.Module):
                                    hidden_size=n_dec_hidden,
                                    num_layers=n_dec_layers,
                                    batch_first=True)
-        self.calc_output = nn.Linear(n_dec_hidden, n_input)
+        self.dec_linear = nn.Linear(n_dec_hidden, n_input)
 
         # Parameters
         self.n_input = n_input
@@ -69,18 +69,17 @@ class VRAE(nn.Module):
         return mu, ln_var
 
     def decode(self, z, dec_inp, inp_len):
-        n_batch = z.size(0)
+        n_batch = dec_inp.size(0)
         packed_in = pack_padded_sequence(dec_inp, inp_len, batch_first=True)
         h_dec = self.gen_z_h(z)
         dec_c0 = torch.zeros(self.n_dec_layers, n_batch, self.n_dec_hidden, requires_grad=True).to(self.get_device())
         packed_out, _ = self.decoder(packed_in, (h_dec.view(self.n_dec_layers, n_batch, self.n_dec_hidden), dec_c0))
         dec_out, out_len = pad_packed_sequence(packed_out, batch_first=True)
-        out, out_len = ensure_padded_sequence(self.calc_output(dec_out), out_len)
-        return out, out_len
+        return self.dec_linear(dec_out), out_len
 
     def generate(self, z, seq_len):
         with torch.no_grad():
-            dec_inp = make_ones(seq_len, self.n_input).to(self.get_device())
+            dec_inp = make_padded_ones(seq_len, self.n_input).to(self.get_device())
             dec_out, out_len = self.decode(z, dec_inp, seq_len)
         return dec_out, out_len
 
@@ -88,12 +87,13 @@ class VRAE(nn.Module):
         mu, ln_var = self.encode(enc_inp, inp_len)
         loss_func = nn.MSELoss(reduction='mean')
         n_batch = enc_inp.size(0)
-        dec_inp = make_ones(inp_len, self.n_input).to(self.get_device())
+        dec_inp = make_padded_ones(inp_len, self.n_input).to(self.get_device())
         rec_loss = 0
         for _ in six.moves.range(k):
             z = torch.normal(mu, ln_var)
             dec_out, out_len = self.decode(z, dec_inp, inp_len)
-            rec_loss += loss_func(enc_inp, dec_out)
+            for o, t in zip(make_unpadded_sequence(dec_out, out_len), make_unpadded_sequence(enc_inp, inp_len)):
+                rec_loss += loss_func(o, t)
         rec_loss /= (k * n_batch)
         kld = -0.5 * torch.sum(1 + ln_var - mu.pow(2) - ln_var.exp()) / n_batch
         return rec_loss + beta * kld

train.py
@@ -6,6 +6,7 @@ import torch.utils.data as tud
 from torch.nn.utils.rnn import pad_sequence
 
 from VRAE import VRAE
+from utils import make_padded_sequence
 import dataset
 
 
@@ -60,14 +61,6 @@ def output_log(epoch, train_loss, test_loss):
     print("{}, {}, {}".format(epoch, train_loss, test_loss), flush=True)
 
 
-def make_batch(x_list, device):
-    x_len = torch.LongTensor([len(x) for x in x_list])
-    x_list = pad_sequence(x_list, batch_first=True)
-    x_len, perm_idx = x_len.sort(0, descending=True)
-    x_list = x_list[perm_idx]
-    return x_list.to(device), x_len.to(device)
-
-
 def train_model():
     args = parse_arg()
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -116,19 +109,19 @@ def train_model():
     for epoch in range(args.epoch):
         train_loss = 0
         for indices in train_iter:
-            x_data, x_len = make_batch([train_dat[idx] for idx in indices], device)
+            x_data, x_len = make_padded_sequence([train_dat[idx] for idx in indices])
             model.zero_grad()
-            loss = model.loss(x_data, x_len, k=1)
+            loss = model.loss(x_data.to(device), x_len.to(device), k=1)
             loss.backward()
             optimizer.step()
-            train_loss += loss * len(x_data)
+            train_loss += loss
 
         # evaluation
         test_loss = 0
         with torch.no_grad():
             for indices in test_iter:
-                x_data, x_len = make_batch([test_dat[idx] for idx in indices], device)
-                test_loss += model.loss(x_data, x_len, k=10) * len(x_data)
+                x_data, x_len = make_padded_sequence([test_dat[idx] for idx in indices])
+                test_loss += model.loss(x_data.to(device), x_len.to(device), k=10)
 
         output_log(epoch, train_loss / len(train_iter), test_loss / len(test_iter))
 

utils.py
@@ -2,10 +2,18 @@ import torch
 from torch.nn.utils.rnn import pad_sequence, pack_padded_sequence, pad_packed_sequence
 
 
-def make_ones(seq_len, n_elements):
-    seq_list = [torch.FloatTensor([[1.0 for _ in range(n_elements)] for _ in range(v)]) for v in seq_len]
-    return pad_sequence(seq_list, batch_first=True)
+def make_padded_ones(seq_len, n_elements):
+    seq = [torch.ones(s, n_elements) for s in seq_len]
+    return pad_sequence(seq, batch_first=True)
 
 
-def ensure_padded_sequence(seq, seq_len):
-    return pad_packed_sequence(pack_padded_sequence(seq, seq_len, batch_first=True), batch_first=True)
+def make_padded_sequence(seq):
+    seq_len = torch.LongTensor([len(s) for s in seq])
+    seq = pad_sequence(seq, batch_first=True)
+    seq_len, perm_idx = seq_len.sort(0, descending=True)
+    seq = seq[perm_idx]
+    return seq, seq_len
+
+
+def make_unpadded_sequence(seq, seq_len):
+    return [seq[i, :l, ...] for i, l in enumerate(seq_len)]