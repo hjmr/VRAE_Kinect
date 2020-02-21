bef2a81cde68d23168859b7bda65c87f5074eccb
hjmr <2620990+hjmr@users.noreply.github.com>
2020-02-21 17:00:31 +0900

    correct error calculation

--- VRAE.py
+++ VRAE.py
@@ -4,7 +4,7 @@ import torch
 import torch.nn as nn
 from torch.nn.utils.rnn import pack_padded_sequence, pad_packed_sequence
 
-from utils import make_ones
+from utils import make_ones, ensure_padded_sequence
 
 
 class VRAE(nn.Module):
@@ -39,7 +39,7 @@ class VRAE(nn.Module):
                                    hidden_size=n_dec_hidden,
                                    num_layers=n_dec_layers,
                                    batch_first=True)
-        self.dec_out = nn.Linear(n_dec_hidden, n_input)
+        self.calc_output = nn.Linear(n_dec_hidden, n_input)
 
         # Parameters
         self.n_input = n_input
@@ -74,14 +74,15 @@ class VRAE(nn.Module):
         h_dec = self.gen_z_h(z)
         dec_c0 = torch.zeros(self.n_dec_layers, n_batch, self.n_dec_hidden, requires_grad=True).to(self.get_device())
         packed_out, _ = self.decoder(packed_in, (h_dec.view(self.n_dec_layers, n_batch, self.n_dec_hidden), dec_c0))
-        out, out_len = pad_packed_sequence(packed_out, batch_first=True)
-        return self.dec_out(out)
+        dec_out, out_len = pad_packed_sequence(packed_out, batch_first=True)
+        out, out_len = ensure_padded_sequence(self.calc_output(dec_out), out_len)
+        return out, out_len
 
     def generate(self, z, seq_len):
         with torch.no_grad():
             dec_inp = make_ones(seq_len, self.n_input).to(self.get_device())
-            dec_out = self.decode(z, dec_inp, seq_len)
-        return dec_out
+            dec_out, out_len = self.decode(z, dec_inp, seq_len)
+        return dec_out, out_len
 
     def loss(self, enc_inp, inp_len, beta=1.0, k=1):
         mu, ln_var = self.encode(enc_inp, inp_len)
@@ -91,7 +92,7 @@ class VRAE(nn.Module):
         rec_loss = 0
         for _ in six.moves.range(k):
             z = torch.normal(mu, ln_var)
-            dec_out = self.decode(z, dec_inp, inp_len)
+            dec_out, out_len = self.decode(z, dec_inp, inp_len)
             rec_loss += loss_func(enc_inp, dec_out)
         rec_loss /= (k * n_batch)
         kld = -0.5 * torch.sum(1 + ln_var - mu.pow(2) - ln_var.exp()) / n_batch
--- utils.py
+++ utils.py
@@ -1,7 +1,11 @@
 import torch
-from torch.nn.utils.rnn import pad_sequence
+from torch.nn.utils.rnn import pad_sequence, pack_padded_sequence, pad_packed_sequence
 
 
 def make_ones(seq_len, n_elements):
     seq_list = [torch.FloatTensor([[1.0 for _ in range(n_elements)] for _ in range(v)]) for v in seq_len]
     return pad_sequence(seq_list, batch_first=True)
+
+
+def ensure_padded_sequence(seq, seq_len):
+    return pad_packed_sequence(pack_padded_sequence(seq, seq_len, batch_first=True), batch_first=True)
