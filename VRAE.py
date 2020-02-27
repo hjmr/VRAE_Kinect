f789511dc31befb01079c5e233eb92fd5e4175c6
hjmr <2620990+hjmr@users.noreply.github.com>
2020-02-28 02:03:46 +0900

    modify name of the output layer.

--- VRAE.py
+++ VRAE.py
@@ -29,7 +29,7 @@ class VRAE(nn.Module):
                                num_layers=n_dec_layers,
                                dropout=dec_dropout,
                                batch_first=True)
-        self.dec_linear = nn.Linear(n_dec_hidden, n_input)
+        self.linear_out = nn.Linear(n_dec_hidden, n_input)
 
         # Parameters
         self.n_input = n_input
@@ -66,7 +66,7 @@ class VRAE(nn.Module):
         packed_out, _ = self.decoder(packed_in, (h_dec.view(self.n_dec_layers, n_batch, self.n_dec_hidden), dec_c0))
         padded_out, out_len = pad_packed_sequence(packed_out, batch_first=True)
         out = unpad_sequence(padded_out, out_len)
-        return [self.dec_linear(s) for s in out]
+        return [self.linear_out(s) for s in out]
 
     def generate(self, z, seq_len):
         with torch.no_grad():
