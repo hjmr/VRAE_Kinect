5cebab958a267d45ecd3856e0ef2185e46d38ca5
hjmr <2620990+hjmr@users.noreply.github.com>
2020-07-22 22:54:06 +0900

    add files to treat logs.

---
+++ ave_std_log.py
@@ -0,0 +1,58 @@
+import os
+import argparse
+
+import pandas as pd
+
+COLUMN_TAGS = ('epoch', 'train/loss', 'test/loss')
+
+
+def parse_arg():
+    parser = argparse.ArgumentParser(description='Calc average and standard deviation from train.logs.')
+    parser.add_argument('-e', '--epoch', type=int, default=10,
+                        help='epochs to be converted.')
+    parser.add_argument('-t', '--tag', type=str, nargs=1,
+                        help='specify a column of the log by the tag. '
+                        'tag should be one from {}'.format(COLUMN_TAGS[:1]))
+    parser.add_argument('-x', '--exclude_original', action='store_true',
+                        help='to exclude original data.')
+    parser.add_argument('-o', '--output', type=str,
+                        help='specify filename to which save CSV.')
+    parser.add_argument('dirs', type=str, nargs='+',
+                        help='directorie name(s) contain log data.')
+    return parser.parse_args()
+
+
+def main():
+    args = parse_arg()
+    tag = args.tag[0]
+    if tag in COLUMN_TAGS:
+        data = pd.DataFrame()
+        for d in args.dirs:
+            file_tag = ''.join(os.path.basename(d).split('_'))
+            file_name = '{}/train.log'.format(d)
+            df = pd.read_csv(file_name,
+                             nrows=args.epoch + 1,
+                             comment='#',
+                             names=[file_tag],
+                             usecols=[COLUMN_TAGS.index(tag)])
+            data = pd.concat([data, df], axis=1)
+        avg = data.mean(axis='columns', skipna=False, numeric_only=True)
+        var = data.var(axis='columns', skipna=False, numeric_only=True)
+        std = data.std(axis='columns', skipna=False, numeric_only=True)
+
+        if args.exclude_original:
+            data = pd.DataFrame()  # clear data
+
+        data['average'] = avg
+        data['variance'] = var
+        data['std_dev'] = std
+
+        data.index.name = 'epoch'
+        if args.output is not None:
+            data.to_csv(args.output)
+        else:
+            print(data)
+
+
+if __name__ == '__main__':
+    main()
--- conv_log.py
+++ conv_log.py
@@ -1,56 +1,50 @@
 import os
 import argparse
 
-COLUMN_TAGS = ['epoch', 'train/loss', 'test/loss']
+import pandas as pd
+
+COLUMN_TAGS = ('epoch', 'train/loss', 'test/loss')
 
 
 def parse_arg():
-    parser = argparse.ArgumentParser(description='Convert train.logs suitable to draw graph.')
-    parser.add_argument('-e', '--epoch', type=int, default=10, help='epochs to be converted.')
-    parser.add_argument('-t', '--tags', type=str, help='specify tag(s) included to log')
-    parser.add_argument('dirs', type=str, nargs='+', help='directorie name(s) contain log data.')
+    parser = argparse.ArgumentParser(description='Calc average and standard deviation from train.logs.')
+    parser.add_argument('-e', '--epoch', type=int, default=10,
+                        help='epochs to be converted.')
+    parser.add_argument('-t', '--tags', type=str, nargs=1,
+                        help='specify columns of the log by comma-seperated-tags. '
+                        'tag(s) should be one or more from {}'.format(COLUMN_TAGS[:1]))
+    parser.add_argument('-o', '--output', type=str,
+                        help='specifyl filename to which save result in CSV.')
+    parser.add_argument('dirs', type=str, nargs='+',
+                        help='directorie name(s) contain log data.')
     return parser.parse_args()
 
 
-def register_data(data_array, tag_array, epoch, item, item_tag):
-    if item_tag not in tag_array:
-        tag_array.append(item_tag)
-    data_array[epoch][item_tag] = item
+def main():
+    args = parse_arg()
+    tags = args.tags[0].split(',')
+    usecols = []
+    for t in tags:
+        if t in COLUMN_TAGS:
+            usecols.append(COLUMN_TAGS.index(t))
 
+    data = pd.DataFrame()
+    for d in args.dirs:
+        file_tag = ''.join(os.path.basename(d).split('_'))
+        file_name = '{}/train.log'.format(d)
+        df = pd.read_csv(file_name,
+                         nrows=args.epoch + 1,
+                         comment='#',
+                         names=[file_tag],
+                         usecols=usecols)
+        data = pd.concat([data, df], axis=1)
 
-def conv_single_log(data_array, tag_array, file_name, file_tag, max_epoch, tags):
-    with open(file_name, 'r') as f:
-        for line in f:
-            if (not line.startswith('#')) and ',' in line:
-                items = line.strip().split(',')
-                epoch = int(items[0])
-                if epoch <= max_epoch:
-                    for idx in range(1, len(items)):
-                        if COLUMN_TAGS[idx] in tags:
-                            item_tag = "{}/{}".format(file_tag, COLUMN_TAGS[idx])
-                            register_data(data_array, tag_array, epoch, items[idx], item_tag)
-
-
-def conv_log(args):
-    data_array = [{} for i in range(args.epoch + 1)]
-    tag_array = []
-    tags = args.tags.split(',') if args.tags is not None else None
-    for dn in args.dirs:
-        file_tag = ''.join(os.path.basename(dn).split('_'))
-        conv_single_log(data_array, tag_array, "{}/train.log".format(dn), file_tag, args.epoch, tags)
-    return data_array, tag_array
+    data.index.name = 'epoch'
+    if args.output is not None:
+        data.to_csv(args.output)
+    else:
+        print(data)
 
 
 if __name__ == '__main__':
-    args = parse_arg()
-    data_array, tag_array = conv_log(args)
-    print('epoch,{}'.format(','.join(tag_array)))
-    for i in range(1, args.epoch+1):
-        line = [str(i)]
-        epoch_data = data_array[i]
-        for t in tag_array:
-            if t in epoch_data:
-                line.append(epoch_data[t])
-            else:
-                line.append('')
-        print(','.join(line))
+    main()
